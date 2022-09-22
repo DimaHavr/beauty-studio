fc0c1d601a46833aefcec3bc410a241c83851940
DimaHavr <109313387+DimaHavr@users.noreply.github.com>
2022-09-23 01:32:59 +0300

    added js to section "blog"

--- js/video.js
+++ js/video.js
@@ -1,3 +1,5 @@
+// Button play/pause
+
 document.addEventListener(
   'DOMContentLoaded',
   function () {
@@ -25,6 +27,19 @@ function togglePlayPause() {
     btn.className = 'play';
     mediaPlayer.pause();
   }
+}
+
+// Input volume
+
+let audio = document.getElementById('media-video');
+
+for (let e of document.querySelectorAll(
+  'input[type="range"].slider-progress'
+)) {
+  e.style.setProperty('--value', e.value);
+  e.style.setProperty('--min', e.min == '' ? '0' : e.min);
+  e.style.setProperty('--max', e.max == '' ? '100' : e.max);
+  e.addEventListener('input', () => e.style.setProperty('--value', e.value));
 }
 
 // Player
